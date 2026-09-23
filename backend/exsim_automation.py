d002f415800788c40806bc1f96804e0ce438c177
Nick444-del <nickgoruleax000e1000@gmail.com>
2026-09-23 12:29:51 +0530

    feat(exsim): add CGST/SGST/IGST @ 9%/9%/18% for Wowtruck Service Fee 100 $ Per Container

--- backend/exsim_automation.py
+++ backend/exsim_automation.py
@@ -211,6 +211,9 @@ def process_exsim_automation(master_csv_path, plant_csv_path, vouchers_dir, base
                     'Additional_Toll': row_data.get('Additional Toll', ''),
                     'Addiotional_Cost_Info': row_data.get('Addiotional Cost Info', ''),
                     'wowtruck_service_fee': row_data.get('Wowtruck Service Fee 100 $ Per Container', row_data.get('Wowtruck Service Fee 100 Per Container', '')),
+                    'wowtruck_service_fee_cgst': row_data.get('CGST (Wowtruck Service Fee 100 $ Per Container) @ 9%', row_data.get('CGST (Wowtruck Service Fee 100 Per Container) @ 9%', '')),
+                    'wowtruck_service_fee_sgst': row_data.get('SGST (Wowtruck Service Fee 100 $ Per Container) @ 9%', row_data.get('SGST (Wowtruck Service Fee 100 Per Container) @ 9%', '')),
+                    'wowtruck_service_fee_igst': row_data.get('IGST (Wowtruck Service Fee 100 $ Per Container) @ 18%', row_data.get('IGST (Wowtruck Service Fee 100 Per Container) @ 18%', '')),
                     'rounding_off': row_data.get('Rounding Off', ''),
                     'dsc_path': os.path.join(base_dir, 'DSC.png'),  # Included just in case
                     'dsc_datetime': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -227,8 +230,9 @@ def process_exsim_automation(master_csv_path, plant_csv_path, vouchers_dir, base
                 charge_keys = [
                     'ocean_freight_charges', 'ocean_freight_cgst', 'ocean_freight_sgst', 'ocean_freight_igst',
                     'carrier_local_charges', 'carrier_local_cgst', 'carrier_local_sgst', 'carrier_local_igst',
+                    'wowtruck_service_fee', 'wowtruck_service_fee_cgst', 'wowtruck_service_fee_sgst', 'wowtruck_service_fee_igst',
                     'wowtruck_handling_charges', 'wowtruck_handling_cgst', 'wowtruck_handling_sgst', 'wowtruck_handling_igst',
-                    'wowtruck_service_fee', 'rounding_off'
+                    'rounding_off'
                 ]
                 
                 calculated_total = sum(parse_amt(row_data.get(k, 0)) for k in charge_keys)
